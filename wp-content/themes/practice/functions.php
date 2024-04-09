e7e545bf990505af6230ad3028af2e185f302b5c
Rene Alejo <ralejomorejon@gmail.com>
2024-04-09 19:11:35 -0400

    Finished last exercise, homepage is showing all the products from the "ropa" category, sry i forgot to put it on english

--- wp-content/themes/practice/functions.php
+++ wp-content/themes/practice/functions.php
@@ -2,7 +2,18 @@
 
 function add_product_loop()
 {
-    echo "<p>Test</p>";
+    echo "<p>Showing all products having \"clothes category\"</p>";
 }
 
-add_action('storefront_page_after', 'add_product_loop');
+add_action('storefront_page', 'add_product_loop');
+
+//funcion para agregar al front-page todos los productos de una categoria
+function add_clothes_products_to_homepage()
+{
+
+    if (is_front_page()) {
+
+        echo do_shortcode('[products category="ropa"]');
+    }
+}
+add_action('storefront_page_after', 'add_clothes_products_to_homepage');
